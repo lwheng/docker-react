0ccdfc8c824be2d31f80971ed7dd1b8465037e9f
Low-Wee Heng <alpha116@gmail.com>
2020-06-18 22:51:16 +0800

    Added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! My name is Barry Allen. Updated!
+          Hello World!
         </p>
         <a
           className="App-link"
